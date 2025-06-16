b14ec0ea54cb929c252604c0a1a7d35068642476
Xu_Mj <40650924+Xu-Mj@users.noreply.github.com>
2025-06-16 10:46:35 +0800

    feat: update down sql

--- migrations/20240322014422_groups.down.sql
+++ migrations/20240322014422_groups.down.sql
@@ -1,1 +1,12 @@
+-- Drop tables in reverse order to respect foreign key constraints
+DROP TABLE IF EXISTS group_announcements;
+DROP TABLE IF EXISTS group_invitations;
+DROP TABLE IF EXISTS group_mute_records;
+DROP TABLE IF EXISTS group_polls;
+DROP TABLE IF EXISTS group_files;
+DROP TABLE IF EXISTS group_members;
+DROP TABLE IF EXISTS group_categories;
 DROP TABLE IF EXISTS groups;
+
+-- Drop custom types
+DROP TYPE IF EXISTS group_role;
